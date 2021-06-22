64e17cde9c3ea08ac5a7e62e0db2d17a960a465b
emanuel <emanueldavidroca@gmail.com>
2021-06-21 22:09:31 -0300

    nuevas imagenes y sobre-nosotros

--- sobre nosotros.php
+++ sobre nosotros.php
@@ -1,5 +1,25 @@
 <?php require_once("header.html");?>
 
-SOBRO NOSOTROS
+<div class="container" id="hero-sobre-nosotros">
+	<div class="row justify-content-center">
+		<div class="col-md-5">
+			<h1 class="text-cloudworks">
+				Sobre <br>
+				nosotros
+			</h1>
+			<div class="row">
+				<div class="col-md-6">
+					<img src="images/connaxis-clean.png" class="w-100" alt="">
+				</div>
+				<div class="col-md-6">
+					<img src="images/PNG-CWS.png" class="w-100" alt="">
+				</div>
+			</div>
+		</div>
+		<div class="col-md-5">
+			<img src="images/sobre-nosotros-hero.png" alt="">
+		</div>
+	</div>
+</div>
 
 <?php require_once("footer.html");?>
