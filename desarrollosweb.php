128a81f211a730c69bcd74e2cac155f9553bd258
emanuel <emanueldavidroca@gmail.com>
2021-07-01 19:49:23 -0300

    desarrollos web

---
+++ desarrollosweb.php
@@ -0,0 +1,210 @@
+<?php require_once("header.html"); ?>
+<div class="container-fluid" id="hero-sobre-nosotros">
+	<div class="row justify-content-end py-4">
+		<div class="col-6 col-md-5 d-flex justify-content-center" style="flex-direction: column;">
+			<h1 class="text-cloudworks px-2">
+				Desarrollo <br>
+				web
+			</h1>
+			<div class="row mt-1 mt-md-5 ps-4 ps-md-0">
+				<div class="col-6 col-md-6 p-0 p-md-3">
+					<img src="images/connaxis-clean.png" class="w-75" alt="">
+				</div>
+				<div class="col-6 col-md-6 p-0 p-md-3">
+					<img src="images/PNG-CWS.png" class="w-75" alt="">
+				</div>
+			</div>
+		</div>
+		<div class="col-4 col-md-6 p-0 p-md-0 d-flex justify-content-center hero-img">
+			<img src="images/Desarrollo web Header-02.png" class="w-100 w-md-75" alt="">
+		</div>
+	</div>
+</div>
+<div class="px-4">
+	<hr>
+</div>
+<div class="container my-3 my-md-5 py-2" id="section-decada">
+	<div class="row justify-content-between">
+		<div class="col-md-5 ps-2 pe-3">
+			<h3 class="title text-cloudworks montserrat-bold">Más de una década</h3>
+			<h4 class="subtitle lato-regular mt-2 mb-4">+10 años impulsando la innovación
+			<br>y la transformación digital.</h4>
+			<p class="lato-light fw-bold">Trabajamos en busca del éxito de nuestros
+			clientes y aprovechamos cualquier posibilidad de
+			transformar el mundo digital. Buscamos jugar un
+			rol activo en la innovación que se genera con la
+			tecnología y contribuir con su desarrollo.</p>
+		</div>
+		<div class="col-md-6">
+			<img src="images/desarrollos web/dw mobile-04.png" class="img-fluid" alt="">
+		</div>
+	</div>
+</div>
+<div class="px-4">
+	<hr>
+</div>
+<div class="container my-3 my-md-5 py-2" id="section-outsorcing">
+	<div class="row justify-content-between">
+		<div class="col-md-5">
+			<img src="images/desarrollos web/dw mobile-06.png" class="img-fluid" alt="">
+		</div>
+		<div class="col-md-6 text-end d-flex align-items-center ps-2 pe-5">
+			<div>
+				<h3 class="title text-cloudworks montserrat-bold">Outsourcing</h3>
+				<h4 class="subtitle lato-regular mt-2 mb-4">¿Pensaste en hacer outsourcing para tus
+				desarrollos?</h4>
+				<p class="lato-light fw-bold">Construimos relaciones a largo plazo con un
+				servicio de Outsourcing que permite enfocarte
+				en el core de tu negocio y disponer de recursos
+				altamente calificados on demand.</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="px-4">
+	<hr>
+</div>
+<div class="container my-3 my-md-5 py-2" id="section-testimonio">
+	<div class="row justify-content-between">
+		<div class="col-md-6 ps-2 pe-3 d-flex align-items-center">
+			<div>
+				<h4 class="subtitle lato-regular mt-2 mb-4">Con nuestras soluciones inteligentes tu <br>	
+				negocio obtiene la más alta calidad y eficiencia.</h4>
+				<p class="lato-light fw-bold">El objetivo es que sientas como si el trabajo lo estuvieran
+				realizando personas de tu propio equipo. <br>
+				Nuestras soluciones inteligentes proveen la mejor relación
+				calidad-precio gracias a nuestra red internacional de
+				oficinas con numerosos recursos de todos los perfiles. <br>
+				La cuidada gestión que hacemos del proyecto y la
+				inteligencia analítica de negocio de Connaxis asegura el
+				control sobre cada actividad y plazos.</p>
+			</div>
+		</div>
+		<div class="col-md-5">
+			<img src="images/desarrollos web/testimonio.png" class="img-fluid" alt="">
+		</div>
+	</div>
+</div>
+<div class="px-4">
+	<hr>
+</div>
+<div class="container my-3 my-md-5 py-2" id="section-diferencia">
+	<div class="row">
+		<div class="col-md-5">
+			<img src="images/desarrollos web/dw mobile-07.png" class="img-fluid" alt="">
+		</div>
+		<div class="col-md-7 ps-2 pe-5 text-end">
+			<h4 class="subtitle lato-regular mt-2 mb-4">¿Cómo marcamos la diferencia?</h4>
+			<p class="lato-light fw-bold">Contamos con una metodología efectiva para el trabajo
+			remoto. Utilizamos las últimas herramientas de colaboración
+			y comunicación digital. Generamos una sinergia orgánica en
+			el modo de trabajar, entre la demanda u objetivos del cliente
+			y nuestro equipo de trabajo.
+			<br>
+			<br>
+			Nuestras oficinas alrededor del mundo cuentan con
+			instalaciones de primer nivel en términos de equipamiento,
+			tecnología, soporte de management y recursos humanos.</p>
+		</div>
+	</div>
+</div>
+<div class="container-fluid bg-cloudworks" id="section-tecnologia">
+	<div class="row justify-content-center px-2">
+		<div class="col-md-5 text-light d-flex align-items-center pe-5 ps-3">
+			<div>
+				<h3 class="title mb-0">Tecnología</h3>
+				<h4 class="subtitle lato-light mb-4">Las más modernas del mercado en
+				un solo lugar.</h4>
+				<p class="lato-light pe-5">Tenemos una amplia diversidad de proyectos
+				donde administramos, de forma integral, y
+				nos ocupamos de definir fechas y tiempos
+				con una metodología ágil que garantiza el
+				éxito.</p>
+			</div>
+		</div>
+
+		<div class="col-md-5">
+			<img src="images/desarrollos web/dw mobile-11.png" class="w-100" alt="">
+		</div>
+	</div>
+</div>
+
+
+
+<div class="container my-3 my-md-5 py-2" id="section-wordpress">
+	<div class="row justify-content-between">
+		<div class="col-md-5 ps-2 pe-3">
+			<h3 class="title text-cloudworks montserrat-bold">Wordpress, el mejor CMS</h3>
+			<p class="lato-light fw-bold">WordPress está siendo utilizado actualmente en más
+			del 30% de sitios web, por lo tanto otorga credibilidad
+			y confiabilidad haciendo que sea la herramienta
+			preferida para prácticamente cualquier tipo de sitio
+			web que se te ocurra.
+			<br><br>
+			Es una plataforma que permite un balance perfecto
+			entre flexibilidad de Desarrollo, Diseño e integración
+			con plataformas de E-Commerce, Sistemas de
+			Gestión, Redes Sociales y otros plugins.</p>
+		</div>
+		<div class="col-md-6">
+			<video class="w-100" controls style="">
+			  <source src="video/GIF-TRABAJOS-CW (1).mp4" type="video/mp4">
+			  <source src="movie.ogg" type="video/ogg">
+			</video>
+		</div>
+	</div>
+</div>
+<div class="container my-3 my-md-5 py-2" id="section-woocommerce">
+	<div class="row justify-content-between">
+		<div class="col-md-6">
+			<img src="images/desarrollos web/Desarrollo web-03.png" class="img-fluid" alt="">
+		</div>
+		<div class="col-md-5 ps-2 pe-3">
+			<h3 class="title text-cloudworks montserrat-bold">Woocommerce</h3>
+			<h4 class="subtitle lato-regular mt-2 mb-4">Vendé más con Woocommerce.</h4>
+			<p class="lato-light fw-bold">Aprovecha a tener tu tienda online con la
+			combinación de WordPress+Woocommerce para
+			potenciar las ventas de tu negocio y mejorar la
+			experiencia de los compradores desde cualquier
+			dispositivo, ya que es tecnología web responsive.
+			<br><br>
+			Woocommerce te permite crear una tienda robusta
+			e integrarla a múltiples medios de pagos líderes en
+			el mercado como Stripe, PayPal, MercadoPago entre
+			otros.
+			<br><br>
+			Podrás dar de alta y baja los productos de una
+			manera muy sencilla, está pensado para
+			emprendedores y empresas con gran envergadura,
+			tiene millones de descargas y funciona con el 30%
+			de sitios de comercio electrónico.</p>
+		</div>
+		
+	</div>
+</div>
+<div class="container my-3 my-md-5 py-2" id="section-seo">
+	<div class="row justify-content-between">
+		<div class="col-md-5 ps-2 pe-3">
+			<h3 class="title text-cloudworks montserrat-bold">SEO Y UIX</h3>
+			<h4 class="subtitle lato-regular mt-2 mb-4">Mejora el SEO y la experiencia de tus usuarios en WP</h4>
+			<p class="lato-light fw-bold">WordPress cuenta con Plugins fáciles de implementar y
+			ahorran tiempo de programación, manteniendo un sitio web
+			sólido y su espíritu autoadministrable para cualquier usuario.
+			Hay 21.000 plugins disponibles y es muy probable que antes
+			de agregar código se pueda añadir un simple plugin.
+			<br><br>
+			WordPress es un CMS que está orientado a mejorar el
+			posicionamiento orgánico con diferentes plugins como Yoast,
+			WP Fasted Cache, WP-Rocket, AMP for WP, entre otros.
+			Además los blogs que podemos manipular el contenido y
+			<br><br>
+			dedicarnos a una estrategia de contenido con foco en posi-
+			cionamiento orgánico.</p>
+		</div>
+		<div class="col-md-6">
+			<img src="images/desarrollos web/Desarrollo web-04.png" class="img-fluid" alt="">
+		</div>
+	</div>
+</div>
+
+<?php require_once("footer.html"); ?>
---
+++ pruebaafter.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="pruebaafter.css">
+</head>
+<body>
+	<h1 class="hero-title"><span class="span-desktop">Creamos sitios web y aplicaciones</span><span class="span-mobile">Creamos sitios web y apps</span></h1>
+
+	<p>Aliquip consectetur aliqua pariatur esse sunt in aliqua nisi incididunt eu sit ex exercitation adipisicing ad ut consectetur duis ea sint qui veniam ut adipisicing <span class="">est</span> duis quis reprehenderit in consectetur.</p>
+	<span style="color:green;">100</span><span style="font-weight: bold;">$</span>
+	
+</body>
+</html>
